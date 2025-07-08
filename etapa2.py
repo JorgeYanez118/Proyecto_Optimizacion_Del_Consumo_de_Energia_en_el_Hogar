221fe4e5161ba364cd75fdf648a49a5a1a548bc7
LuisALema <118556807+LuisALema@users.noreply.github.com>
2025-07-07 21:50:32 -0500

    etapa2.1

---
+++ etapa2.py
@@ -0,0 +1,16 @@
+# ETAPA 2 – ANÁLISIS DEL CONSUMO ELÉCTRICO (USANDO ARCHIVOS DIVIDIDOS)
+# --------------------------------------------------------------------
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# 1. Cargar y unir los archivos .txt
+partes = []
+for i in range(1, 11):
+    archivo = f"datos_limpios_parte_{i}.txt"
+    df_parte = pd.read_csv(archivo, sep=';')
+    partes.append(df_parte)
+
+df = pd.concat(partes, ignore_index=True)
+
